8faf34e15e0160e7d165ff9004a8bfc5edbafd82
Kinco <colin.kindt@gmail.com>
2025-02-05 19:36:20 +0100

    feat(client): add waitCoinToss

--- packages/core/src/client/client.ts
+++ packages/core/src/client/client.ts
@@ -1,9 +1,10 @@
 import { type Config as WagmiConfig } from "@wagmi/core";
-import type { Hash, Hex } from "viem";
+import type { Hex, TransactionReceipt } from "viem";
 import { type ChainId } from "../data/chains.ts";
 import {
   placeCoinTossBet,
   type CoinTossInputs,
+  type CoinTossPlacedBet,
 } from "../actions/casino/cointoss.ts";
 import type { CasinoChainId } from "../data/casino.ts";
 import { casinoChainById } from "../data/casino.ts";
@@ -11,6 +12,11 @@ import { ChainError } from "../errors/types.ts";
 import { ERROR_CODES } from "../errors/codes.ts";
 import type { GAS_PRICE_TYPE } from "../read/gasPrice.ts";
 import type { ALLOWANCE_TYPE } from "../actions/common/approve.ts";
+import type { CasinoWaitRollOptions } from "../read/casino/game.ts";
+import {
+  waitCoinTossRolledBet,
+  type CoinTossRolledBet,
+} from "../read/casino/cointoss.ts";
 
 export interface BetSwirlClientOptions {
   gasPriceType?: GAS_PRICE_TYPE;
@@ -33,7 +39,28 @@ export class BetSwirlClient {
     this.betSwirlDefaultOptions = betSwirlDefaultOptions;
   }
 
-  async playCoinToss(inputs: CoinTossInputs): Promise<Hash> {
+  async playCoinToss(
+    inputs: CoinTossInputs
+  ): Promise<{ placedBet: CoinTossPlacedBet; receipt: TransactionReceipt }> {
+    const chainId = this._getCasinoChainId();
+    return placeCoinTossBet(
+      this.wagmiConfig,
+      { ...inputs, affiliate: this.betSwirlDefaultOptions.affiliate },
+      {
+        ...this.betSwirlDefaultOptions,
+        chainId: chainId as CasinoChainId | undefined,
+      }
+    );
+  }
+
+  async waitCoinToss(
+    placedBet: CoinTossPlacedBet,
+    options: CasinoWaitRollOptions
+  ): Promise<{ rolledBet: CoinTossRolledBet; receipt: TransactionReceipt }> {
+    return waitCoinTossRolledBet(this.wagmiConfig, placedBet, options);
+  }
+
+  _getCasinoChainId(): CasinoChainId | undefined {
     const chainId = this.betSwirlDefaultOptions.chainId;
     if (chainId && !(chainId in casinoChainById)) {
       throw new ChainError(
@@ -45,14 +72,7 @@ export class BetSwirlClient {
         }
       );
     }
-    return placeCoinTossBet(
-      { ...inputs, affiliate: this.betSwirlDefaultOptions.affiliate },
-      this.wagmiConfig,
-      {
-        ...this.betSwirlDefaultOptions,
-        chainId: chainId as CasinoChainId,
-      }
-    );
+    return chainId as CasinoChainId | undefined;
   }
 
   static init(
--- packages/core/src/read/casino/cointoss.ts
+++ packages/core/src/read/casino/cointoss.ts
@@ -1,17 +1,21 @@
 import { type Config as WagmiConfig } from "@wagmi/core";
 import { type TransactionReceipt } from "viem";
-import type { CasinoRolledBet, CasinoWaitRollOptions } from "./game.ts";
+import {
+  waitRolledBet,
+  type CasinoRolledBet,
+  type CasinoWaitRollOptions,
+} from "./game.ts";
 import {
   decodeCoinTossInput,
   type COINTOSS_FACE,
   type CoinTossPlacedBet,
 } from "../../actions/casino/cointoss.ts";
 
-interface CoinTossRolledBet extends CasinoRolledBet {
+export interface CoinTossRolledBet extends CasinoRolledBet {
   rolled: COINTOSS_FACE[];
 }
 
-export async function waitRolledBet(
+export async function waitCoinTossRolledBet(
   wagmiConfig: WagmiConfig,
   placedBet: CoinTossPlacedBet,
   options?: CasinoWaitRollOptions
