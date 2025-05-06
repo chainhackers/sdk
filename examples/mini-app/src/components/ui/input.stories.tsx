498706b860a5ebf936093d227e1b6a7b31d18931
Nikita Bednyakov <bednyakov99@gmail.com>
2025-05-06 16:07:32 +0300

    refactor: remove unused input stories

--- examples/mini-app/src/components/ui/input.stories.tsx
+++ examples/mini-app/src/components/ui/input.stories.tsx
@@ -29,26 +29,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    placeholder: 'Default Input',
-  },
-};
-
-export const Disabled: Story = {
-  args: {
-    placeholder: 'Disabled Input',
-    disabled: true,
-  },
-};
-
-export const NumberInput: Story = {
-  args: {
-    type: 'number',
-    placeholder: 'Enter a number',
-  },
-};
-
-export const WithTokenInfo: Story = {
-  args: {
     type: 'number',
     placeholder: '0',
     value: '10.5',
@@ -58,3 +38,17 @@ export const WithTokenInfo: Story = {
     },
   },
 };
+
+export const Disabled: Story = {
+  args: {
+    type: 'number',
+    placeholder: '0',
+    value: '10.5',
+    token: {
+      icon: <Cog className="h-4 w-4 mr-1 text-orange-500" />,
+      symbol: "POL"
+    },
+    disabled: true,
+  },
+};
+
