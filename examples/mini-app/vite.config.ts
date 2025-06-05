631ef1aa42fa750a99194d21d1635353eb218833
helisar <nsav248@gmail.com>
2025-06-05 09:53:21 +0300

    chore: move dependencies for library #27

--- examples/mini-app/vite.config.ts
+++ examples/mini-app/vite.config.ts
@@ -3,6 +3,13 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+const assetFileNames = (assetInfo: any) => {
+  if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+    return "index.css";
+  }
+  return "assets/[name][extname]";
+};
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -15,54 +22,36 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "ChainhackersUI",
-      formats: ["es", "cjs"],
     },
     rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "@betswirl/sdk-core",
-        "@coinbase/onchainkit",
-        "@radix-ui/react-dialog",
-        "@radix-ui/react-label",
-        "@radix-ui/react-popover",
-        "@radix-ui/react-scroll-area",
-        "@radix-ui/react-slider",
-        "@radix-ui/react-slot",
-        "@tailwindcss/vite",
-        "@tanstack/react-query",
-        "class-variance-authority",
-        "clsx",
-        "loki",
-        "lucide-react",
-        "tailwind-merge",
-        "tailwindcss",
-        "tw-animate-css",
-        "viem",
-        "wagmi",
-      ],
+      external: (id) => {
+
+        const externalPackages = [
+          "react",
+          "react-dom",
+          "@betswirl/sdk-core",
+          "@coinbase/onchainkit",
+          "@tanstack/react-query",
+          "viem",
+          "wagmi"
+        ];
+        
+        return externalPackages.some(pkg => 
+          id === pkg || id.startsWith(pkg + "/")
+        );
+      },
       output: [
         {
           format: "es",
           entryFileNames: "index.mjs",
           chunkFileNames: "chunks/[name].mjs",
-          assetFileNames: (assetInfo) => {
-            if (assetInfo.name && assetInfo.name.endsWith(".css")) {
-              return "index.css";
-            }
-            return "assets/[name][extname]";
-          },
+          assetFileNames,
         },
         {
           format: "cjs",
           entryFileNames: "index.js",
           chunkFileNames: "chunks/[name].js",
-          assetFileNames: (assetInfo) => {
-            if (assetInfo.name && assetInfo.name.endsWith(".css")) {
-              return "index.css";
-            }
-            return "assets/[name][extname]";
-          },
+          assetFileNames,
         },
       ],
     },
