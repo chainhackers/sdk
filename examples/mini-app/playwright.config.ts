849f1f79f3f71f98f2d2b7d521e875686a2c490c
helisar <nsav248@gmail.com>
2025-06-06 22:42:16 +0300

    chore{tests): setup synpress and playwright #63

---
+++ examples/mini-app/playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: false,
+  forbidOnly: false,
+  retries: 0,
+  workers: 1,
+  reporter: 'html',
+  timeout: 120 * 1000,
+  use: {
+    baseURL: 'http://localhost:5173',
+    trace: 'on-first-retry',
+    headless: false,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  // Additional Synpress-specific configuration can be added here
+})
