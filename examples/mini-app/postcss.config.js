f5ed8ca7508b2008c3c1e3cca23dcd86be02fb76
helisar <nsav248@gmail.com>
2025-06-19 13:12:04 +0300

    chore: return inlining fonts #27

--- examples/mini-app/postcss.config.js
+++ examples/mini-app/postcss.config.js
@@ -1,17 +1,9 @@
 import tailwindcss from "@tailwindcss/postcss"
 import autoprefixer from "autoprefixer"
 
-import fs from "node:fs"
-import path from "node:path"
-import { fileURLToPath } from "node:url"
-import postcss from "postcss"
 import postcssImport from "postcss-import"
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const fontsPath = path.resolve(__dirname, "src", "fonts.css")
-
 const isProd = process.env.NODE_ENV === "production"
-let isLayersUnpacked = false
 
 /** @type {import('postcss').ProcessOptions} */
 export default {
@@ -34,19 +26,6 @@ export default {
                   }
                 }
               })
-              isLayersUnpacked = true
-            },
-          },
-          {
-            postcssPlugin: "append-fonts",
-            OnceExit(root) {
-              if (!isLayersUnpacked) {
-                return
-              }
-
-              const fontsCSS = fs.readFileSync(fontsPath, "utf-8")
-              const fontsRoot = postcss.parse(fontsCSS, { from: fontsPath })
-              root.append(fontsRoot)
             },
           },
         ]
