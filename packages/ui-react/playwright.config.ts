8f8153a42e7cc84cc05af2fd8aa4d56cf8484fc9
etsvigun <gene@chainhackers.xyz>
2025-07-10 13:45:18 +0300

    fix: remove unsupported defineSynpressConfig import #124

--- packages/ui-react/playwright.config.ts
+++ packages/ui-react/playwright.config.ts
@@ -1,26 +1,23 @@
 import { defineConfig } from "@playwright/test"
-import { defineSynpressConfig } from "@synthetixio/synpress"
 import { config } from "./app.config"
 
-export default defineConfig(
-  defineSynpressConfig({
-    testDir: "./tests",
-    timeout: 120000,
-    fullyParallel: false,
-    forbidOnly: false,
-    retries: 2,
-    workers: 1,
-    reporter: "html",
-    expect: {
-      timeout: 7000,
-    },
-    use: {
-      headless: false,
-      chromiumChannel: "chromium",
-      baseURL: config.baseUrl,
-      actionTimeout: 60000,
-      trace: "on-first-retry",
-      video: "retain-on-failure",
-    },
-  }),
-)
+export default defineConfig({
+  testDir: "./tests",
+  timeout: 120000,
+  fullyParallel: false,
+  forbidOnly: false,
+  retries: 2,
+  workers: 1,
+  reporter: "html",
+  expect: {
+    timeout: 7000,
+  },
+  use: {
+    headless: false,
+    chromiumChannel: "chromium",
+    baseURL: config.baseUrl,
+    actionTimeout: 60000,
+    trace: "on-first-retry",
+    video: "retain-on-failure",
+  },
+})
