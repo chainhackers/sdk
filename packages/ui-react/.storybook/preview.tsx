5e66c755c45ffb37a91f103fbfe6e7b9077c2ed5
helisar <nsav248@gmail.com>
2025-07-21 21:29:47 +0300

    chore: remove manager.ts, extract StorybookVersionWrapper #192

--- packages/ui-react/.storybook/preview.tsx
+++ packages/ui-react/.storybook/preview.tsx
@@ -1,39 +1,13 @@
 import type { Decorator, Preview } from "@storybook/react"
 import "../src/index.css"
 import { StorybookProviders } from "../src/storybook/StorybookProviders"
+import { StorybookVersionWrapper } from "../src/storybook/StorybookVersionWrapper"
 
 const withAppProviders: Decorator = (Story) => (
   <StorybookProviders>
     <Story />
   </StorybookProviders>
 )
-
-const withVersionOnHeader: Decorator = (Story) => {
-  //@ts-ignore
-  const version = import.meta.env.VITE_STORYBOOK_VERSION
-  return (
-    <>
-      {version && (
-        <div
-          style={{
-            display: "inline-block",
-            background: "#f2f2f2",
-            padding: "4px 16px",
-            fontSize: "12px",
-            fontWeight: "bold",
-            marginBottom: "10px",
-            borderRadius: "4px",
-            color: "#333333",
-            letterSpacing: 1,
-          }}
-        >
-          {version}
-        </div>
-      )}
-      <Story />
-    </>
-  )
-}
 
 const preview: Preview = {
   parameters: {
@@ -49,7 +23,7 @@ const preview: Preview = {
       },
     },
   },
-  decorators: [withVersionOnHeader, withAppProviders],
+  decorators: [StorybookVersionWrapper, withAppProviders],
 }
 
 export default preview
