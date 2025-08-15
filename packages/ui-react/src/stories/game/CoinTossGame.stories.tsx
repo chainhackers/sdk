0228c5b84cb605ace7d4785658b0522e7e71beb0
helisar <nsav248@gmail.com>
2025-08-15 19:49:30 +0300

    chore: remove unused usePlaceFreebet hook #208

--- packages/ui-react/src/stories/game/CoinTossGame.stories.tsx
+++ packages/ui-react/src/stories/game/CoinTossGame.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/react-vite"
-import type { FreeBet } from "../../components/game/BettingPanel"
 import { CoinTossGame, type CoinTossGameProps } from "../../components/game/CoinTossGame"
 import { STORYBOOK_TOKENS, StorybookProviders } from "../../storybook/StorybookProviders"
 import { THEME_OPTIONS } from "../../types/types"
